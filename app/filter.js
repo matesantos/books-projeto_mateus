710aabdbeb0048588cfad0da0de8019af02b7b11
Mateus Santos <matesantos@hotmail.com>
2023-05-10 00:33:36 -0300

    projeto finalizado

--- app/filter.js
+++ app/filter.js
@@ -1,13 +1,33 @@
-const btnFiltrarLivrosFront = document.querySelector("#btnFiltrarLivrosFront");
-const btnFiltrarLivrosBack = document.querySelector("#btnFiltrarLivrosBack");
-const btnFiltrarLivrosDados = document.querySelector("#btnFiltrarLivrosDados");
-
 const btnFilter = document.querySelectorAll('.btn');
 
 btnFilter.forEach(btn => btn.addEventListener('click', filterBooks));
 
 function filterBooks() {
     const element = document.querySelector(`#${this.id}`);
-    const booksFiltered =  books.filter(book => book.categoria === element.value);
+    const categoria = element.value;
+
+    console.log(categoria)
+
+    const booksFiltered = categoria === 'disponivel' ? filterAvailableBooks() : filterByCategory(element);
     showTheBooksOnTheScreen(booksFiltered);
+    categoria === 'disponivel' && exibirValorTotalDosLivrosDisponiveisNaTela(booksFiltered);
+}
+
+function filterByCategory(element) {
+    return books.filter(book => book.categoria === element.value);
+}
+
+function filterAvailableBooks() {
+    return books.filter(book => book.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(booksFiltered){
+    console.table(booksFiltered)
+    const totalValue = booksFiltered.reduce((acc, book) => acc + book.preco, 0);
+
+    elementoComValorTotalDeLivrosDisponveis.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">${totalValue.toFixed(2)}</span></p>
+        </div>
+    `
 }
--- app/forEach.js
+++ app/forEach.js
@@ -1,11 +1,14 @@
 const elementoParaInserirLivros = document.getElementById('livros');
+const elementoComValorTotalDeLivrosDisponveis  = document.getElementById('valor_total_livros_disponiveis');
 
 function showTheBooksOnTheScreen(listOfBooks) {
+    elementoComValorTotalDeLivrosDisponveis.innerHTML = ""
     elementoParaInserirLivros.innerHTML = ""
     listOfBooks.forEach(book => {
+        let bookAvailable = checkAvailableBook(book);
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-            <img class="livro__imagens" src="${book.imagem}" alt="${book.alt}" />
+            <img class="${bookAvailable}" src="${book.imagem}" alt="${book.alt}" />
             <h2 class="livro__titulo">${book.titulo}</h2>
             <p class="livro__descricao">${book.autor}</p>
             <p class="livro__preco" id="preco">R$ ${book.preco.toFixed(2)}</p>
@@ -15,4 +18,8 @@ function showTheBooksOnTheScreen(listOfBooks) {
         </div>
         `
     });
+}
+
+function checkAvailableBook(book){
+    return book.quantidade > 0 ? 'livro__imagens': 'livro__imagens indisponivel';
 }
